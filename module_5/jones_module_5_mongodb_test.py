b39fef905f16019c2632f5e105958334cd1d19a1
Blackmanq <95770111+Blackmanq@users.noreply.github.com>
2022-09-20 01:32:14 +0200

    Update jones_module_5_mongodb_test.py

--- module_5/jones_module_5_mongodb_test.py
+++ module_5/jones_module_5_mongodb_test.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
-connection = MongoClient("mongodb+srv://admin:admin@cluster0.inz66kj.mongodb.net/pytech")
-pytechdb = connection["pytech"]
-studentcollection = pytechdb["students"]
+connection = "mongodb+srv://admin:admin@cluster0.inz66kj.mongodb.net/pytech"
+client = MongoClient(connection)
+mydb = client.pytech
+pytechdb = mydb["pytech"]
+studentcollection = mydb.students
 
 print("-- pytech Collection List --")
 print(["students"])
